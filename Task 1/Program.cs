e476a538ce8c311aebe740d45bfe9be144acf3e6
Shreyabasnet <basnets349@gmail.com>
2025-12-02 11:39:02 +0545

    Task1 and Task2

---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle
+        {
+            Length = 10,
+            Breadth = 5
+        };
+
+        Console.WriteLine(rect.ShowDetails());
+        Console.WriteLine("Area: " + rect.GetArea());
+        Console.WriteLine("Perimeter: " + rect.GetPerimeter());
+    }
+}
---
+++ Task 1/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿public class Rectangle
+{
+    private double length;
+    private double breadth;
+
+    public double Length
+    {
+        get => length;
+        set => length = value;
+    }
+
+    public double Breadth
+    {
+        get => breadth;
+        set => breadth = value;
+    }
+
+    public double GetArea() => Length * Breadth;
+
+    public double GetPerimeter() => 2 * (Length + Breadth);
+
+    public string ShowDetails() =>
+        $"Length: {Length}, Breadth: {Breadth}, Area: {GetArea()}, Perimeter: {GetPerimeter()}";
+}
