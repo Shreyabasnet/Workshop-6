d43e474e14658edd168112b560de67e2d2690796
Shreyabasnet <basnets349@gmail.com>
2025-12-02 12:11:31 +0545

    Task3

--- Task 2/obj/Debug/net9.0/Task 2.AssemblyInfo.cs
+++ Task 2/obj/Debug/net9.0/Task 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e476a538ce8c311aebe740d45bfe9be144acf3e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
